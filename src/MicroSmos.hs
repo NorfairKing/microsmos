9ba9587d771c8cbc56863cddc17153ac0f94b1ff
Tom Sydney Kerckhove <syd@cs-syd.eu>
2019-05-20 19:36:39 +0200

    A full blown tree editor

--- src/MicroSmos.hs
+++ src/MicroSmos.hs
@@ -19,6 +19,7 @@ import System.Exit
 
 import Cursor.Text
 import Cursor.Tree
+import Cursor.Types
 
 import Data.Tree
 import Data.Yaml as Yaml
@@ -45,8 +46,9 @@ microSmos = do
         case Yaml.decodeEither' contents of
           Left err -> die $ "Failed to read tree file: " <> prettyPrintParseException err
           Right tree -> pure $ makeTreeCursor toTextCursor (cTree True (tree :: Tree Text))
-  tc' <- Brick.defaultMain microSmosApp tc
-  SB.writeFile file $ Yaml.encode $ rebuildCTree $ rebuildTreeCursor toText tc'
+  let s = State {stateCursor = tc, stateMode = EditTree}
+  s' <- Brick.defaultMain microSmosApp s
+  SB.writeFile file $ Yaml.encode $ rebuildCTree $ rebuildTreeCursor toText $ stateCursor s'
 
 toTextCursor :: Text -> TextCursor
 toTextCursor = fromMaybe (error "Wasn't a single line") . makeTextCursor
@@ -54,7 +56,17 @@ toTextCursor = fromMaybe (error "Wasn't a single line") . makeTextCursor
 toText :: TextCursor -> Text
 toText = rebuildTextCursor
 
-type State = TreeCursor TextCursor Text
+data State =
+  State
+    { stateCursor :: TreeCursor TextCursor Text
+    , stateMode :: Mode
+    }
+  deriving (Show, Eq)
+
+data Mode
+  = EditTree
+  | EditText
+  deriving (Show, Eq)
 
 microSmosApp :: App State e ResourceName
 microSmosApp =
@@ -63,12 +75,65 @@ microSmosApp =
     , appChooseCursor = showFirstCursor
     , appHandleEvent = handleEvent
     , appStartEvent = pure
-    , appAttrMap = const $ attrMap Vty.defAttr []
+    , appAttrMap =
+        const $
+        attrMap
+          Vty.defAttr
+          [ (nodeAttr, fg yellow)
+          , (selectedAttr, fg brightWhite)
+          , (modeAttr, fg green)
+          , (infoAttr, fg blue)
+          , (warningAttr, fg red)
+          ]
     }
 
 draw :: State -> [Widget ResourceName]
-draw tc = [centerLayer $ border $ padAll 1 $ drawTreeCursor wrap cur tc]
+draw s =
+  [ padAll 1 $
+    vBox
+      [ withAttr nodeAttr $ padBottom Max $ drawTreeCursor wrap cur $ stateCursor s
+      , hBorder
+      , withAttr modeAttr drawMode
+      , hBorder
+      , withAttr infoAttr drawInfo
+      , hBorder
+      , hCenterLayer $
+        withAttr warningAttr $
+        strWrap
+          "Note that this is a tree editor, not a forest editor, so there is no way to add a sibling to the root node."
+      ]
+  ]
   where
+    drawMode :: Widget n
+    drawMode =
+      str $
+      case stateMode s of
+        EditText -> "Editing selected node's text"
+        EditTree -> "Editing tree"
+    drawInfo :: Widget n
+    drawInfo =
+      let explanations =
+            map (\(k, v) -> (str k, str v)) $
+            case stateMode s of
+              EditText ->
+                [ ("<char>", "insert character")
+                , ("Backspace", "remove character")
+                , ("Delete", "delete character")
+                , ("Left", "move left within node")
+                , ("Right", "move right within node")
+                , ("Enter, Esc", "Switch mode")
+                ]
+              EditTree ->
+                [ ("i, I, a, A", "switch mode")
+                , ("j", "Add a sibling node")
+                , ("J", "Add a child node")
+                , ("d", "Delete selected node")
+                , ("D", "Delete selected subtree")
+                , ("Up", "move to previous node")
+                , ("Down", "move to next node")
+                , ("Esc", "Exit")
+                ]
+       in (\(ks, vs) -> hBox $ [vBox ks, padLeft (Pad 2) $ vBox vs]) $ unzip explanations
     drawTreeCursor ::
          forall a b n.
          ([CTree b] -> b -> [CTree b] -> Widget n -> Widget n)
@@ -87,22 +152,25 @@ draw tc = [centerLayer $ border $ padAll 1 $ drawTreeCursor wrap cur tc]
           wrapAboveFunc (reverse treeAboveLefts) treeAboveNode treeAboveRights
     cur :: TextCursor -> CForest Text -> Widget ResourceName
     cur ec cf =
-      case cf of
-        EmptyCForest -> drawTextCursor ec
-        ClosedForest _ -> drawTextCursor ec
-        OpenForest ts ->
-          let ecw = drawTextCursor ec
-              etws = map drawTextCTree $ NE.toList ts
-           in ecw <=> padLeft defaultPadding (vBox etws)
+      let ecw = withAttr selectedAttr $ str "> " <+> drawTextCursor ec
+          rest =
+            case cf of
+              EmptyCForest -> emptyWidget
+              ClosedForest _ -> emptyWidget
+              OpenForest ts ->
+                let etws = map drawTextCTree $ NE.toList ts
+                 in padLeft defaultPadding (vBox etws)
+       in vBox [ecw, rest]
     wrap :: [CTree Text] -> Text -> [CTree Text] -> Widget n -> Widget n
     wrap tsl e tsr w =
       let befores = map drawTextCTree tsl
           ew = txt e
           afters = map drawTextCTree tsr
-       in ew <=> padLeft defaultPadding (vBox $ concat [befores, [w], afters])
+       in (str "- " <+> ew) <=> padLeft defaultPadding (vBox $ concat [befores, [w], afters])
 
 drawTextCTree :: CTree Text -> Widget n
 drawTextCTree (CNode t cf) =
+  str "- " <+>
   case cf of
     EmptyCForest -> txt t
     ClosedForest _ -> txt t
@@ -114,7 +182,25 @@ drawTextCTree (CNode t cf) =
 drawTextCursor :: TextCursor -> Widget ResourceName
 drawTextCursor tc =
   visible . showCursor TextResource (Brick.Location (textCursorIndex tc, 0)) $
-  txt $ rebuildTextCursor tc
+  txt $
+  case rebuildTextCursor tc of
+    "" -> " "
+    t -> t
+
+nodeAttr :: AttrName
+nodeAttr = "node"
+
+selectedAttr :: AttrName
+selectedAttr = "selected"
+
+modeAttr :: AttrName
+modeAttr = "mode"
+
+infoAttr :: AttrName
+infoAttr = "info"
+
+warningAttr :: AttrName
+warningAttr = "warning"
 
 defaultPadding :: Padding
 defaultPadding = Pad 4
@@ -124,22 +210,54 @@ data ResourceName =
   deriving (Show, Eq, Ord)
 
 handleEvent :: State -> BrickEvent n e -> EventM n (Next State)
-handleEvent tc e =
+handleEvent s e =
   case e of
     VtyEvent ve ->
       case ve of
         EvKey key mods ->
           let textDo :: (TextCursor -> Maybe TextCursor) -> (EventM n (Next State))
-              textDo func = continue . fromMaybe tc $ (treeCursorCurrentL func) tc
-              mDo :: (State -> Maybe State) -> (EventM n (Next State))
-              mDo func = continue . fromMaybe tc $ func tc
-           in case key of
-                KChar c -> textDo $ textCursorInsert c
-                KBS -> textDo textCursorRemove
-                KDel -> textDo textCursorDelete
-                KLeft -> textDo textCursorSelectPrev
-                KRight -> textDo textCursorSelectNext
-                KEsc -> halt tc
-                _ -> continue tc
-        _ -> continue tc
-    _ -> continue tc
+              textDo func = mDo $ treeCursorCurrentL func
+              mDo ::
+                   (TreeCursor TextCursor Text -> Maybe (TreeCursor TextCursor Text))
+                -> (EventM n (Next State))
+              mDo func =
+                let tc = stateCursor s
+                    tc' = fromMaybe tc $ func tc
+                 in continue $ s {stateCursor = tc'}
+           in case stateMode s of
+                EditText ->
+                  let switchMode = continue $ s {stateMode = EditTree}
+                   in case key of
+                        KChar c -> textDo $ textCursorInsert c
+                        KBS -> textDo textCursorRemove
+                        KDel -> textDo textCursorDelete
+                        KLeft -> textDo textCursorSelectPrev
+                        KRight -> textDo textCursorSelectNext
+                        KEnter -> switchMode
+                        KEsc -> switchMode
+                        _ -> continue s
+                EditTree ->
+                  let switchMode = continue $ s {stateMode = EditText}
+                   in case key of
+                        KChar 'i' -> switchMode
+                        KChar 'I' -> switchMode
+                        KChar 'a' -> switchMode
+                        KChar 'A' -> switchMode
+                        KChar 'j' -> mDo $ treeCursorAppend (Node "" [])
+                        KChar 'J' -> mDo $ Just . treeCursorAddChildAtStart (Node "" [])
+                        KChar 'd' ->
+                          mDo $ \tc ->
+                            case treeCursorRemoveElem toTextCursor tc of
+                              Deleted -> Nothing
+                              Updated tc' -> Just tc'
+                        KChar 'D' ->
+                          mDo $ \tc ->
+                            case treeCursorRemoveSubTree toTextCursor tc of
+                              Deleted -> Nothing
+                              Updated tc' -> Just tc'
+                        KUp -> mDo $ treeCursorSelectPrev toText toTextCursor
+                        KDown -> mDo $ treeCursorSelectNext toText toTextCursor
+                        KEsc -> halt s
+                        _ -> continue s
+        _ -> continue s
+    _ -> continue s
